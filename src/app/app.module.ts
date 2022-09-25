d8a45ae2fb983ddc39501e02a6faccd54522d238
yuuchwa <78650989+yuuchwa@users.noreply.github.com>
2022-09-25 18:40:25 +0200

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { SingleSlotContentComponent } from './single-slot-content/single-slot-content.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, SingleSlotContentComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/single-slot-content/single-slot-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-single-slot-content',
+  templateUrl: './single-slot-content.component.html',
+  styleUrls: ['./single-slot-content.component.css']
+})
+export class SingleSlotContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
